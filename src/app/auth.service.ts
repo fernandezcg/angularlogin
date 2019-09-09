4bc7912b32d06f45a9e53b0eb6d9b824f17101bc
cesarfg <cesar.fg@gmail.com>
2019-09-09 17:53:25 +0200

    login 2

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getUserDetails() {
-    
+  getUserDetails(username, password) {
+    let params = new HttpParams();
+       params = params.append('name', username);
+       params = params.append('password', password);
+
+    return this.http.get('/api/users', {
+      params
+    })
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
   }
 
   loginUser(event) {
    event.preventDefault()
-   console.log(event) 
+   const target = event.target
+   const username = target.querySelector('#username').value
+   const password = target.querySelector('#password').value
+
+   this.auth.getUserDetails(username, password).subscribe(data => {
+    console.log(data, " is what we got from server.")
+    if (data[0].name) {
+      console.log("login success")
+    } else {
+      console.log("login fail")
+    }
+  })
+
+   console.log(username, password) 
   }
 
 }
